be2c2af5ee373e8b5fff67a3f72fa49b8edb778c
WagnerReis <wagnerreis1997@gmail.com>
2021-08-17 11:59:25 -0300

    Relacionamentos nos Models

--- src/database/migrations/1629205880129-CreateUsers.ts
+++ src/database/migrations/1629205880129-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1629205880129 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
